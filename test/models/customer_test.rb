a0b1edbf9c68f62a791589c181002733d1e2cc82
Felipe Armoni <felipe.armoni@gmail.com>
2017-05-29 19:44:26 -0300

    Added test fixtures and a test for customer model.

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class CustomerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should validate the e-mail" do
+
+    #(name: "Jake Blair", email: "jake.blair@bradesco.com.br", company_id: bradesco.id)
+    customer = Customer.new(name: "Paul Atreides", email: "paul.atreides@dune.com", company_id: Company.find_by(name: "Bradesco").id)
+    assert customer.valid?
+
+    customer = Customer.new(name: "Miles Teg", email: "miles.tegdune.com", company_id: Company.find_by(name: "Bradesco").id)
+    assert_not customer.valid?
+
+  end
+
 end
